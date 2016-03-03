439c1d984773b7c88496d072e8da9b859a430aa7
Andreas Hauptmann <fdopen@users.noreply.github.com>
2016-03-03 12:24:59 +0100

    load initial environment

--- appveyor-opam.sh
+++ appveyor-opam.sh
@@ -6,8 +6,11 @@ curl -fsSL -o opam64.tar.xz "https://dl.dropboxusercontent.com/s/b2q2vjau7if1c1b
 tar -xf opam64.tar.xz
 opam64/install.sh
 opam init -a mingw "https://github.com/fdopen/opam-repository-mingw.git" --comp 4.02.3+mingw64c --switch 4.02.3+mingw64c
+export CYGWIN='winsymlinks:native'
+eval $(opam config env)
+export PATH="/usr/x86_64-w64-mingw32/sys-root/mingw/bin:${PATH}"
 opam install depext-cygwinports depext
-
+eval $(opam config env)
 opam pin add my-pkg "${APPVEYOR_BUILD_FOLDER}" -n -k path
 opam depext  my-pkg
 opam install my-pkg
